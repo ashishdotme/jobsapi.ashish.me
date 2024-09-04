12bc9b4daefe1c804df6e155795a045ca41b8c15
Ashish Patel <ashishsushilpatel@gmail.com>
2024-09-04 19:13:28 +0100

    feat: update listens controller

--- src/listens/listens.controller.ts
+++ src/listens/listens.controller.ts
@@ -1,24 +1,35 @@
-import { Controller, Post, Body, Query, Request } from '@nestjs/common';
+import { Controller, Post, Body, Query, Request, Get, HttpCode } from '@nestjs/common';
 import { ListensService } from './listens.service';
 // import { CreateListenDto } from './dto/create-listen.dto';
 import { ApiTags, ApiSecurity } from '@nestjs/swagger';
+import { ok } from 'assert';
 
-@Controller('/apis/listenbrainz')
+@Controller('/1')
 @ApiTags('listens')
 @ApiSecurity('apiKey')
 export class ListensController {
   constructor(private readonly listensService: ListensService) {}
 
-  @Post()
+  @Get("validate-token")
+  @HttpCode(200)
+  validateToken(
+    @Request() req,
+    @Body() createListeDto: any,
+    @Query('apikey') apiKeyParam: string,
+  ) {
+    return {
+			code: 200,
+			message: 'Token valid',
+			valid: true
+		}
+  }
+
+  @Post("submit-listens")
   create(
     @Request() req,
     @Body() createListeDto: any,
     @Query('apikey') apiKeyParam: string,
   ) {
-    const apiKey = apiKeyParam || req.headers.apikey;
-    if (!apiKey) {
-      return { error: 'Apikey cannot be blank' };
-    }
-    return this.listensService.create(createListeDto, apiKey);
+    console.log(createListeDto)
   }
 }
