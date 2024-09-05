0c4dc7311f33b70acfe974299e8787696a5c4bd3
Ashish Patel <ashishsushilpatel@gmail.com>
2024-09-05 16:51:37 +0000

    refactor: cleanup

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -11,7 +11,7 @@ export class TransactionsService {
       const category = transactionCategories.find((x) =>
         x.keywords.includes(createTransactionDto.merchant),
       );
-      if(!category){
+      if (!category) {
         await sendEvent(
           'create_transaction_failed',
           createTransactionDto.merchant,
