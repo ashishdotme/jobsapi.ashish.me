a49eb8d9c71108f48764a622de850c9b97ed4b2f
Ashish Patel <ashishsushilpatel@gmail.com>
2024-10-24 20:59:11 +0100

    feat: handle sleep data

--- src/metrics/metrics.service.ts
+++ src/metrics/metrics.service.ts
@@ -6,26 +6,50 @@ import { CreateMetricDto } from './dto/create-metric.dto';
 @Injectable()
 export class MetricsService {
 	async create(createMetricDto: CreateMetricDto, apiKey: string) {
-		const metrics = createMetricDto.data.metrics.find(data => data.name === 'step_count');
-		for (const step of metrics.data) {
+		const stepsMetrics = createMetricDto.data.metrics.find(data => data.name === 'step_count');
+		for (const step of stepsMetrics.data) {
 			const newMetric = {
 				stepCount: step.qty.toFixed(),
 				date: format(step.date.split(' ')[0], 'M/d/yy'),
 				fullDate: new Date(step.date.split(' ')[0]),
 			};
-			await this.postMetrics(newMetric, apiKey);
+			await this.postSteps(newMetric, apiKey);
+		}
+		const sleepMetrics = createMetricDto.data.metrics.find(data => data.name === 'sleep_analysis');
+		for (const sleep of sleepMetrics.data) {
+			const newMetric = {
+				sleep: sleep.inBed,
+				date: format(sleep.date.split(' ')[0], 'M/d/yy'),
+				fullDate: new Date(sleep.date.split(' ')[0]),
+				sleepStart: sleep.sleepStart,
+				sleepEnd: sleep.sleepEnd,
+			};
+			await this.postSleep(newMetric, apiKey);
 		}
 		return HttpStatus.OK;
 	}
-	private async postMetrics(newMovie: any, apikey: string): Promise<any> {
-		// use try catch block to handle errors
+	private async postSteps(newStep: any, apikey: string): Promise<any> {
 		try {
 			const config = {
 				headers: {
 					apikey: apikey,
 				},
 			};
-			const response = await axios.post('https://api.ashish.me/steps', newMovie, config);
+			const response = await axios.post('https://api.ashish.me/steps', newStep, config);
+			return response.data;
+		} catch (error) {
+			console.error(error.response.data);
+		}
+	}
+
+	private async postSleep(newSleep: any, apikey: string): Promise<any> {
+		try {
+			const config = {
+				headers: {
+					apikey: apikey,
+				},
+			};
+			const response = await axios.post('https://api.ashish.me/sleep', newSleep, config);
 			return response.data;
 		} catch (error) {
 			console.error(error.response.data);
